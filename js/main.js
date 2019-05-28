f1278b36e3270845a65131edd76f50079aa09c27
Genaro Gonzalez <genarrogg@gmail.com>
2019-05-28 17:38:33 -0400

    Efecto Menu

--- js/main.js
+++ js/main.js
@@ -26,3 +26,22 @@ $(document).ready(function(){
 	});
 
 });
+
+$(document).ready(function(){
+
+    // Efecto Menu
+        $('.menu a').each(function(index, elemento){
+            $(this).css({
+                'top': '-200px'
+            });
+    
+            $(this).animate({
+                top: '0'
+            }, 2000 + (index * 500));
+        });
+    
+
+     
+        
+    
+    });
