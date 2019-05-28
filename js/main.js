a1f78462599af77a43cb2aa967711401aa8ed9ce
Genaro Gonzalez <genarrogg@gmail.com>
2019-05-28 17:34:52 -0400

    parallax

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		var windowWidth = $(window).width();
+
+		if(windowWidth > 800){
+			var scroll = $(window).scrollTop();
+
+			$('header .textos').css({
+				'transform': 'translate(0px,' + scroll / 2 + '%)'
+			});
+
+			$('.acerca-de article').css({
+				'transform': 'translate(0px, -' + scroll / 4 + '%)'
+			});
+		}
+	});
+
+	
+});
